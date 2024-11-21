045a1448eab2440f27f1603acc5cae520a403864
Ashish Shah <eiepixxstuff@gmail.com>
2024-11-21 19:37:38 +0530

    add middleware to the code base

---
+++ auth.next.js/src/app/tempuser/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function TempUser(){
+    return (
+      <div className="flex h-lvh justify-center items-center text-3xl ">
+        <h1 className="bg-slate-400 px-4 py-2 rounded-xl"> This is the special page that without log in you can&apos;t visit it.</h1>
+      </div>
+    );
+}
